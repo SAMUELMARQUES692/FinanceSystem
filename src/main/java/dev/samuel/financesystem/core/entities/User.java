0e3a906f17bde0c1080d318cc4c9c2221b1848ce
samuel <samue@Samuel>
2026-08-16 17:12:18 -0300

    feat: Cria um metodo responsvel para criar o Builder na classe User

--- src/main/java/dev/samuel/financesystem/core/entities/User.java
+++ src/main/java/dev/samuel/financesystem/core/entities/User.java
@@ -12,4 +12,53 @@ public record User(
         LocalDateTime createdAt,
         List<Scope> scopes
 ) {
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private Long id;
+        private String name;
+        private String email;
+        private String password;
+        private LocalDateTime createdAt;
+        private List<Scope> scopes;
+
+        public Builder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder email(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public Builder password(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public Builder createdAt(LocalDateTime createdAt) {
+            this.createdAt = createdAt;
+            return this;
+        }
+
+        public Builder scopes(List<Scope> scopes) {
+            this.scopes = scopes;
+            return this;
+        }
+
+        public User build() {
+            return new User(id, name, email, password, createdAt, scopes);
+        }
+
+    }
+
 }
