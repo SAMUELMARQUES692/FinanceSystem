02e34a88c458e4bc1d4824c01e225be1cbce8616
samuel <samue@Samuel>
2026-08-16 15:49:31 -0300

    feat: Cria um metodo responsavel por criar o Builder da entidade de Scope do pacote de core

--- src/main/java/dev/samuel/financesystem/core/entities/Scope.java
+++ src/main/java/dev/samuel/financesystem/core/entities/Scope.java
@@ -4,4 +4,29 @@ public record Scope(
         Long id,
         String name
 ) {
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+   public static class Builder {
+       private Long id;
+       private String name;
+
+
+       public Builder id(Long id) {
+           this.id = id;
+           return this;
+       }
+
+       public Builder name(String name) {
+           this.name = name;
+           return this;
+       }
+
+       public Scope build() {
+           return new Scope(id, name);
+       }
+
+   }
 }
