9b6fe4ccd458146c3ac2d86bc90300020e6c8b62
samuel <samue@Samuel>
2026-08-16 13:52:44 -0300

    feat: Cria uma logica para criar builders na classe Account que esta dentro do core

--- src/main/java/dev/samuel/financesystem/core/entities/Account.java
+++ src/main/java/dev/samuel/financesystem/core/entities/Account.java
@@ -11,4 +11,55 @@ public record Account(
         String agency,
         String number,
         LocalDateTime createdAt
-) implements Serializable {}
+
+) implements Serializable {
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private Long id;
+        private Long userId;
+        private BigDecimal balance;
+        private String agency;
+        private String number;
+        private LocalDateTime createdAt;
+
+        public Builder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder userId(Long userId) {
+            this.userId = userId;
+            return this;
+        }
+
+        public Builder balance(BigDecimal balance) {
+            this.balance = balance;
+            return this;
+        }
+
+        public Builder agency(String agency) {
+            this.agency = agency;
+            return this;
+        }
+
+        public Builder number(String number) {
+            this.number = number;
+            return this;
+        }
+
+        public Builder createdAt(LocalDateTime createdAt) {
+            this.createdAt = createdAt;
+            return this;
+        }
+
+        public Account build() {
+            return new Account(id, userId, balance, agency, number, createdAt);
+        }
+    }
+
+
+}
