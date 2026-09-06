dab1355ae8936118c7c331e9b309010720aeeb8f
samuel <samue@Samuel>
2026-09-06 17:25:44 -0300

    fix: Adiciona uma dependencia de jackson datatype no pom.xml, substitui o caminho da java publica e privada por uma variavel de ambiente, corrige um erro no TestcontainersConfiguration e adiciona um container descartavel para simular o Redis nos testes unitarios na classe BaseIntegrationTest

---
+++ src/test/java/dev/samuel/financesystem/configuration/BaseIntegrationTest.java
@@ -0,0 +1,47 @@
+package dev.samuel.financesystem.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+import static org.springframework.boot.autoconfigure.cache.CacheType.REDIS;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Testcontainers
+@Transactional
+public class BaseIntegrationTest {
+
+    @Container
+    static final PostgreSQLContainer<?> POSTGRES = new PostgreSQLContainer<>("postgres:14-alpine")
+            .withDatabaseName("finance-system")
+            .withUsername("financeSystem")
+            .withPassword("financeSystem");
+
+    @Container
+    static final GenericContainer<?> REDIS = new GenericContainer<>(DockerImageName.parse("redis:7-alpine"))
+            .withExposedPorts(6379);
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", POSTGRES::getJdbcUrl);
+        registry.add("spring.datasource.username", POSTGRES::getUsername);
+        registry.add("spring.datasource.password", POSTGRES::getPassword);
+
+        registry.add("spring.data.redis.host", REDIS::getHost);
+        registry.add("spring.data.redis.port", () -> REDIS.getMappedPort(6379));
+    }
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+}
