d9088ea9d5f8178e909054aef30bc78c177f9954
samuel <samue@Samuel>
2026-09-08 15:53:05 -0300

    feat: Tira a responsabilidade de excluir o container da anotação @Testcontainers e @Containers e faz isso manualmente, deixando a responsabilidade de limpeza para outro cointeiner auxiliar Ryuk, fazendo isso no final de uma sessao inteira, em vez de classe a classe como o JUnit

--- src/test/java/dev/samuel/financesystem/configuration/BaseIntegrationTest.java
+++ src/test/java/dev/samuel/financesystem/configuration/BaseIntegrationTest.java
@@ -9,27 +9,26 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
-
-import static org.springframework.boot.autoconfigure.cache.CacheType.REDIS;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@Testcontainers
 @Transactional
 public class BaseIntegrationTest {
 
-    @Container
     static final PostgreSQLContainer<?> POSTGRES = new PostgreSQLContainer<>("postgres:14-alpine")
             .withDatabaseName("finance-system")
             .withUsername("financeSystem")
             .withPassword("financeSystem");
 
-    @Container
+
     static final GenericContainer<?> REDIS = new GenericContainer<>(DockerImageName.parse("redis:7-alpine"))
             .withExposedPorts(6379);
+
+    static {
+        POSTGRES.start();
+        REDIS.start();
+    }
 
     @DynamicPropertySource
     static void configureProperties(DynamicPropertyRegistry registry) {
